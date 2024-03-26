7abe4815f4b9746de77c27bbfd53c452b78e2d10
ashok rao potta <ashokraopotta@gmail.com>
2024-03-26 21:05:47 +0530

    Update main-common.tf
    For storage account creation set allow_nested_items_to_be_public = false

--- terraform/main-common.tf
+++ terraform/main-common.tf
@@ -85,6 +85,7 @@ resource "azurerm_storage_account" "storageaccount" {
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  allow_nested_items_to_be_public = false
   tags                     = local.tags
 }
 
